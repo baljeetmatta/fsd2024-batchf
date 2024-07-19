41ad33216449b43629a6bb02b0c9e7c5765d9068
RebootTalents <168439890+RebootTalents@users.noreply.github.com>
2024-07-19 13:44:48 +0530

    Objects

--- js/script.js
+++ js/script.js
@@ -26,16 +26,16 @@
 
 //foreach, filter, map
 
-function printData(item,index)
-{
-    console.log("Called",item);
-}
+// function printData(item,index)
+// {
+//     console.log("Called",item);
+// }
 
 //arr.forEach(printData);
 // arr.forEach(function(item){
 //     console.log(item);
 // })
-let arr=[23,4,5,6,7];
+//let arr=[23,4,5,6,7];
 //let results=[];
 // for(i=0;i<arr.length;i++)
 //     {
@@ -63,13 +63,75 @@ results.push(item);
 
 
 })*/
-let results=arr.map(function(item){
-//     if(item%2==0)
-//     return item*2;
-// else
-// return item;
-    return (item%2==0?item*2:item);
+// let results=arr.map(function(item){
+// //     if(item%2==0)
+// //     return item*2;
+// // else
+// // return item;
+//     return (item%2==0?item*2:item);
     
 
+// })
+// console.log(results);
+//OBJECTS
+//let arr=[];
+
+//let obj={'your name':'Code','age':30,'name':'Test','10th':450}
+//console.log(obj.10th);
+//console.log(obj["your name"]);
+//console.log(obj["10th"]);
+
+// //console.log(obj["name"]);
+// let pro="name";
+// //console.log(obj.pro);
+// console.log(obj[pro]);
+
+// for(x in obj)
+//     {
+
+//         console.log(x,":",obj[x]);
+
+//     }
+
+// let obj={
+//     name:'Code',
+//     age:30,
+//     education:['X','XII','B.Sc','Msc'],
+//     marks:{'X':450,'XII':510}
+// }
+
+// console.log(obj.education[0]);
+// obj.education.forEach(function(item){
+//     console.log(item);
+// })
+//obj.marks.X
+
+// let obj={
+//     name:'Test',
+//     age:10,
+//     getName:getMyName
+// }
+// let obj1={
+//     name:'Code',
+//     age:10,
+//     getName:getMyName
+// }
+// function getMyName(){
+//     console.log(this.name);
+
+// }
+// obj1.getName();
+
+// let obj={};
+// obj.name="Code";
+// console.log(obj.age);
+// obj.age=10;
+// console.log(obj);
+// delete obj.age;
+// console.log(obj);
+
+let arr=[{name:'Test',age:30},{name:'Code',age:31}];
+arr.forEach(function(item){
+    console.log(item.name);
+
 })
-console.log(results);
