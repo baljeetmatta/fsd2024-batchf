bcf9d9a50833cc3e44fde8968e49b1876fbfd7d6
RebootTalents <168439890+RebootTalents@users.noreply.github.com>
2024-07-24 14:51:25 +0530

    Dom events

---
+++ js/dom.js
@@ -0,0 +1,44 @@
+//console.log(document);
+//console.dir(document);
+//console.log(document.all);
+//console.log(document.all[3])
+// let elements=document.getElementsByTagName("p");
+// console.log(elements);
+// // elements.forEach(element => {
+// //     console.log(element);
+
+// // });
+// // for(i=0;i<elements.length;i++)
+// //     console.log(elements[i]);
+
+// //console.log(elements[0].innerText);
+// elements[0].innerHTML="New javascript <b>Hello</b>"
+//let mypara=document.getElementById("mypara");
+//mypara.innerHTML="New javascript <b>Hello</b>"
+// let elements=document.getElementsByClassName("x");
+// console.log(elements);
+//queryselector
+// let elements=document.querySelectorAll("p#x")
+// //console.log(elements);
+// elements.forEach(function(item){
+//     console.log(item);
+
+// })
+
+// let box=document.querySelector("#box");
+// box.value="Initial";
+//let mypara=document.querySelector("#mypara");
+//mypara.setAttribute("align","center");
+//mypara.setAttribute("class","x");
+//mypara.classList.add("x");
+//mypara.classList.toggle("y");
+
+let firstpara=document.querySelector("#firstpara");
+firstpara.addEventListener("click",test);
+
+function test()
+{
+    console.log("Para clicked");
+
+}
+
